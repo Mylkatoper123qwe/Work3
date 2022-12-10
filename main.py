57e30a711e1fb93f1d90021e386c29c3a46f8e6e
nshs33-102 <ualixzzy@gmail.com>
2022-12-10 12:32:05 +0200

    COmit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+class People:
+    def __init__(self,name ="People", job=None , home=None , car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
+    def get_home(self):
+        pass
+    def get_car(self):
+        pass
+    def get_job(self):
